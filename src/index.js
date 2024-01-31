be191f1e026228f810dede2b199168cdb44d319c
Karim Khalifeh <karim.ah.khalifeh@gmail.com>
2024-01-31 21:16:35 +0400

    integrating posthog

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PostHogProvider } from "posthog-js/react";
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <PostHogProvider
+    apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+    options={options}
+  >
+    <App />
+  </PostHogProvider>
+);
